6e0779ff860b947b5974d159028fbdaaf5ce19bc
Ryo <89829215+Ryoishin@users.noreply.github.com>
2021-09-02 18:20:48 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ sendTG "Docker image build was killed!"
 trap exit_script SIGINT SIGTERM
 
 function sendTG() {
-    curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=-1001372533112&parse_mode=Markdown"
+    curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=-1001505466119&parse_mode=Markdown"
 }
 
 sendTG "\`Docker image is being updated!\`"
